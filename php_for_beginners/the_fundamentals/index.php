c3ecca3a8fe4571dd04ced6f02bfaaf90018358e
Daniel Soares <daniel.soares@fortics.com.br>
2024-05-10 20:01:22 -0300

    php to begginers: Conditionals and Booleans.

--- php_for_beginners/the_fundamentals/index.php
+++ php_for_beginners/the_fundamentals/index.php
@@ -5,13 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Fundamentals</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 
 <body>
-    <h1><?php
-        $greeting = "Hello";
-        print "$greeting World!"
-        ?></h1>
+    <?php
+    $name = "Dark Matter";
+    $read =  (bool) rand(0, 1);
+
+    if ($read) {
+        $message = "You have read $name.";
+    } else {
+        $message = "You have not read $name";
+    }
+    ?>
+    <h1>
+        <?= $message ?>
+    </h1>
 </body>
 
 </html>
